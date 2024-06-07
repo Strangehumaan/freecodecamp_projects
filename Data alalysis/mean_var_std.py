7543ca21165e90ff99f90af3deafed3e39634c28
Saad Nathani <144052986+Strangehumaan@users.noreply.github.com>
2024-06-07 13:11:23 +0530

    Update mean_var_std.py

--- Data alalysis/mean_var_std.py
+++ Data alalysis/mean_var_std.py
@@ -1,23 +1,33 @@
+# importing the library 
 import numpy as np
 
+#defining Function
 def calculor(list):
     if len(list) != 9:
         raise ValueError("List must contain nine numbers.")
     else:
         array = np.array(list).reshape(3,3)
 
+    
+    # Calculating mean
     mean = [array.mean(axis=0,dtype=float).tolist(),array.mean(axis=1,dtype=float).tolist(),array.mean(dtype=float).tolist()]
 
+    # Calculating Variance
     variance = [array.var(axis=0,dtype=float).tolist(),array.var(axis=1,dtype=float).tolist(),array.var(dtype=float).tolist()]
 
+    #Calculating Standard deviation
     standard_deviation = [array.std(axis=0,dtype=float).tolist(),array.std(axis=1,dtype=float).tolist(),array.std(dtype=float).tolist()]
 
+    #Find the max value
     max = [array.max(axis=0).tolist(),array.max(axis=1).tolist(),array.max().tolist()]
 
+    #Find the min value
     min = [array.min(axis=0).tolist(),array.min(axis=1).tolist(),array.min().tolist()]
 
+    #Find the sum
     sum = [array.sum(axis=0).tolist(),array.sum(axis=1).tolist(),array.sum().tolist()]
 
+    #Gettting all values in the dictionary 
     dic = {'Mean':mean,
             'Variance':variance,
             'Standard deviation':standard_deviation,
